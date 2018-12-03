d368631fd738013dd0e1c76f3481f11d238765a1
Nalla Govindareddy Gar Gunasekhar Reddy <gunasekhar_reddy2@toyota.ca>
2018-12-03 20:13:52 +0000

    Issues correcting the CSRF errors.

--- claimSystemProcessing_Node/router/routes/myNode.js
+++ claimSystemProcessing_Node/router/routes/myNode.js
@@ -10,7 +10,7 @@ var JWTStrategy = require('@sap/xssec').JWTStrategy;
 var async = require('async');
 
 // vehicle Locator Node Module. 
-module.exports = function() {
+module.exports = function () {
 	var app = express.Router();
 
 	// SAP Calls Start from here
@@ -26,8 +26,8 @@ module.exports = function() {
 		url = options.api.host,
 		APIKey = options.api.APIKey,
 		client = options.api.client;
-     	console.log('The API Management URL', url);
 
+	console.log('The API Management URL', url);
 
 	var auth64 = 'Basic ' + new Buffer(uname + ':' + pwd).toString('base64');
 
@@ -41,47 +41,62 @@ module.exports = function() {
 	app.use(function (req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" );
+		
 		next();
 	});
+
+	var csrfToken;
+
+		app.all('/*', function (req, res, next) {
 	
-	// simple version of bridge call
-	app.all('/*', function(req, res, next) {
-
-    	let headOptions = {};
-			var csrfToken;
-		// prepare the magic reverse proxy header for odata service to work. 
-        // let originalHost = req.hostname;
-        // if (!!req.headers.host){
-        //     headOptions.host = req.headers.host;
-        // }
-
-		//only support the basic auth
-        headOptions.Authorization = auth64;
-
-        let method = req.method;
-        let xurl = url +req.url;
-         console.log('Method', method);
-        console.log('Incoming Url', xurl);
-        let xRequest = 
-			request({
-				method : method,
-				url : xurl,
-				headers: reqHeader
- 			}
-		);
-        
-        req.pipe(xRequest);
-
-        xRequest.on('response', (response) => {
-        	// delete response.headers['set-cookie'];
-        		csrfToken = response.headers['x-csrf-token'];
-        	xRequest.pipe(res);
-        }).on('error', (error) => {
-        	next(error);
-       });
-	});
+			let headOptions = {};
 	
+			headOptions.Authorization = auth64;
+	
+			let method = req.method;
+			let xurl = url + req.url;
+			console.log('Method', method);
+			console.log('Incoming Url', xurl);
+			console.log('csrfToken before GET&POST', csrfToken);
+	
+			//  if the method = post you need a csrf token.   
+	
+			if (method == 'POST' || method == 'DELETE' || method == 'PUT') {
+				reqHeader = {
+					"Authorization": auth64,
+					"Content-Type": "application/json",
+					"APIKey": APIKey,
+					"x-csrf-token": csrfToken
+				};
+				console.log('csrfToken for POST', csrfToken);	
+				console.log('headerData', reqHeader);
+			}
+	
+			let xRequest =
+				request({
+					method: method,
+					url: xurl,
+					headers: reqHeader
+				});
+	
+			req.pipe(xRequest);
+	
+			xRequest.on('response', (response) => {
+				// delete response.headers['set-cookie'];
+				if (response.headers['x-csrf-token']){
+				csrfToken = response.headers['x-csrf-token'];
+				}
+				console.log('Response from sap Received Success for', method);
+			
+				xRequest.pipe(res);
+			}).on('error', (error) => {
+				next(error);
+			});
+		});
+
 	return app;
 };
+
 
 
