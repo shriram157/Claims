0b797782a51419ee537d40fc7a2de8f9c8defe0a
Nalla Govindareddy Gar Gunasekhar Reddy <gunasekhar_reddy2@toyota.ca>
2018-12-06 15:38:19 +0000

    Node Modifications for fetch CSRF Token issues

--- claimSystemProcessing_Node/router/routes/myNode.js
+++ claimSystemProcessing_Node/router/routes/myNode.js
@@ -9,6 +9,10 @@ var JWTStrategy = require('@sap/xssec').JWTStrategy;
 
 var async = require('async');
 
+var app = express();
+
+// Use the session middleware
+
 // vehicle Locator Node Module. 
 module.exports = function () {
 	var app = express.Router();
@@ -17,7 +21,7 @@ module.exports = function () {
 	var options = {};
 	options = Object.assign(options, xsenv.getServices({
 		api: {
-			name: "CLAIM_SYSTEM_PROCESSING_CUPS" 
+			name: "CLAIM_SYSTEM_PROCESSING_CUPS"
 		}
 	}));
 
@@ -41,62 +45,87 @@ module.exports = function () {
 	app.use(function (req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS" );
-		
+		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
 		next();
 	});
 
 	var csrfToken;
 
-		app.all('/*', function (req, res, next) {
-	
-			let headOptions = {};
-	
-			headOptions.Authorization = auth64;
-	
-			let method = req.method;
-			let xurl = url + req.url;
-			console.log('Method', method);
-			console.log('Incoming Url', xurl);
-			console.log('csrfToken before GET&POST', csrfToken);
-	
-			//  if the method = post you need a csrf token.   
-	
-			if (method == 'POST' || method == 'DELETE' || method == 'PUT') {
-				reqHeader = {
-					"Authorization": auth64,
-					"Content-Type": "application/json",
-					"APIKey": APIKey,
-					"x-csrf-token": csrfToken
-				};
-				console.log('csrfToken for POST', csrfToken);	
-				console.log('headerData', reqHeader);
-			}
-	
-			let xRequest =
-				request({
-					method: method,
-					url: xurl,
-					headers: reqHeader
-				});
-	
-			req.pipe(xRequest);
-	
-			xRequest.on('response', (response) => {
-				// delete response.headers['set-cookie'];
-				if (response.headers['x-csrf-token']){
-				csrfToken = response.headers['x-csrf-token'];
-				}
-				console.log('Response from sap Received Success for', method);
-			
-				xRequest.pipe(res);
-			}).on('error', (error) => {
-				next(error);
+	app.all('/*', function (req, res, next) {
+
+		let headOptions = {};
+
+		headOptions.Authorization = auth64;
+
+		let method = req.method;
+		let xurl = url + req.url;
+		console.log('Method', method);
+		console.log('Incoming Url', xurl);
+		console.log('csrfToken before GET&POST', csrfToken);
+
+		// console.log(req.headers.cookie);
+		//  delete (req.headers.cookie);
+		//   console.log(req.headers.cookie);
+
+		if (method == 'GET') {
+			//	 delete (req.headers.cookie);  // TODO: to be revisited, if this solvs
+
+			var reqHeader = {
+				"Authorization": auth64,
+				"Content-Type": "application/json",
+				"APIKey": APIKey,
+				"x-csrf-token": "Fetch"
+			};
+
+		}
+
+		//  if the method = post you need a csrf token.   
+
+		if (method == 'POST' || method == 'DELETE' || method == 'PUT' || method == 'HEAD') {
+			reqHeader = {
+				"Authorization": auth64,
+				"Content-Type": "application/json",
+				"APIKey": APIKey,
+				"x-csrf-token": csrfToken
+			};
+			console.log('csrfToken for POST', csrfToken);
+			console.log('headerData', reqHeader);
+		}
+
+		let xRequest =
+			request({
+				method: method,
+				url: xurl,
+				headers: reqHeader
 			});
+
+		req.pipe(xRequest);
+
+		xRequest.on('response', (response) => {
+
+			delete(response.headers.cookie);
+
+			if (response.headers['x-csrf-token']) {
+				if (response.headers['x-csrf-token'] !== 'Required') {
+					csrfToken = response.headers['x-csrf-token'];
+					console.log("csrfToken received from SAP");
+				} else {
+					console.log("Csrf is received as Required.");
+				}
+
+			}
+			console.log("csrfToken NOT received for", method);
+			console.log('Response from sap Received Success for', method);
+
+			xRequest.pipe(res);
+
+		}).on('error', (error) => {
+			next(error);
 		});
+
+	});
 
 	return app;
 };
-
-
 
