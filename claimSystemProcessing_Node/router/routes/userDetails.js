404570108a237020c50b2fbb804e86e3d88e6baa
Nalla Govindareddy Gar Gunasekhar Reddy <gunasekhar_reddy2@toyota.ca>
2019-01-29 03:05:15 +0000

    Adding Route to userDetails to facilitate localDevelopment Testing as InternalUser

--- claimSystemProcessing_Node/router/routes/userDetails.js
+++ claimSystemProcessing_Node/router/routes/userDetails.js
@@ -280,14 +280,6 @@ app.use(log.logNetwork);
     // call with multiple requests. 
     
     app.get("/currentScopesForUser", (req, res) => {
-		// List of scopes:
-		// $XSAPPNAME.Manage_Service_Claims
-		// $XSAPPNAME.Manage_Parts_Claims
-		// $XSAPPNAME.View_Service_Claims
-		// $XSAPPNAME.View_Parts_Claims
-		// $XSAPPNAME.View_Claim_Inquiry
-		// $XSAPPNAME.View_Quick_Coverage_Tool
-		// $XSAPPNAME.View_Dealer_Labor_Rate
 
 		var xsAppName = uaa.xsappname
 		var userAttributes = JSON.parse(JSON.stringify(req.authInfo.userAttributes));
@@ -359,5 +351,158 @@ app.use(log.logNetwork);
 		return res.type("text/plain").status(200).send(JSON.stringify(sendUserData));
 	});
 
+
+	app.get("/attributesforlocaltesting", (req, res) => {
+
+		var receivedData = {};
+
+		var sendToUi = {
+			"attributes": [],
+			"samlAttributes": [],
+			legacyDealer: "",
+			legacyDealerName: ""
+
+		};
+
+		// ===================only for local testing - remove next deploy
+		var obj_temp = {
+			Language: ['English', 'English'],
+			UserType: ['National', 'National'],
+			DealerCode: [' ', ' ']
+		};
+		// console.log(req.authInfo.userAttributes);
+		var parsedData = JSON.stringify(obj_temp);
+		//		 console.log('After Json Stringify', parsedData);
+		var obj_parsed = JSON.parse(parsedData);
+		sendToUi.samlAttributes.push(obj_parsed);
+
+		// =========================================
+
+		//	var parsedData = JSON.stringify(req.authInfo.userAttributes);
+
+		//	var obj = JSON.stringify(req.authInfo.userAttributes);
+		//		var obj_parsed = JSON.parse(obj);
+		var csrfToken;
+		var obj_data = JSON.parse(parsedData);
+		var csrfToken;
+		var samlData = parsedData;
+
+		//	console.log('saml data', samlData);
+
+		//		console.log('send to ui data', sendToUi);
+
+		let checkSAMLDetails;
+		try {
+			checkSAMLDetails = obj_data.DealerCode[0];
+		} catch (e) {
+
+			// return;
+			var nosamlData = true;
+		}
+		sendToUi.samlAttributes.push(obj_parsed);
+
+		var userType = obj_data.UserType[0];
+
+		if (userType == 'Dealer') {
+			var legacyDealer = obj_data.DealerCode[0];
+		}
+
+		//	if  usertype eq dealer then just get the details for that dealer,  otherwise get everything else
+
+		if (userType == 'Dealer') {
+
+			var url1 = "/API_BUSINESS_PARTNER/A_BusinessPartner/?$format=json&$filter=SearchTerm2 eq'" + legacyDealer +
+				"' &$expand=to_Customer&$format=json&?sap-client=" + client;
+
+		} else {
+
+			var url1 = "/API_BUSINESS_PARTNER/A_BusinessPartner/?$format=json&$expand=to_Customer&?sap-client=" + client +
+				"&$filter=(BusinessPartnerType eq 'Z001' or BusinessPartnerType eq 'Z004' or BusinessPartnerType eq 'Z005') and zstatus ne 'X' &$orderby=BusinessPartner asc";
+
+		}
+
+		request({
+			url: url + url1,
+			headers: reqHeader
+
+		}, function (error, response, body) {
+
+			var attributeFromSAP;
+			if (!error && response.statusCode == 200) {
+				csrfToken = response.headers['x-csrf-token'];
+
+				var json = JSON.parse(body);
+				// console.log(json);  // // TODO: delete it Guna
+
+				for (var i = 0; i < json.d.results.length; i++) {
+
+					receivedData = {};
+
+					var BpLength = json.d.results[i].BusinessPartner.length;
+					receivedData.BusinessPartnerName = json.d.results[i].OrganizationBPName1;
+					receivedData.BusinessPartnerKey = json.d.results[i].BusinessPartner;
+					receivedData.BusinessPartner = json.d.results[i].BusinessPartner.substring(5, BpLength);
+					receivedData.BusinessPartnerType = json.d.results[i].BusinessPartnerType;
+					receivedData.SearchTerm2 = json.d.results[i].SearchTerm2;
+
+					let attributeFromSAP;
+					try {
+						attributeFromSAP = json.d.results[i].to_Customer.Attribute1;
+					} catch (e) {
+
+						// return;
+					}
+
+					switch (attributeFromSAP) {
+					case "01":
+						receivedData.Division = "10";
+						receivedData.Attribute = "01"
+						break;
+					case "02":
+						receivedData.Division = "20";
+						receivedData.Attribute = "02"
+						break;
+					case "03":
+						receivedData.Division = "Dual";
+						receivedData.Attribute = "03"
+						break;
+					case "04":
+						receivedData.Division = "10";
+						receivedData.Attribute = "04"
+						break;
+					case "05":
+						receivedData.Division = "Dual";
+						receivedData.Attribute = "05"
+						break;
+					default:
+						receivedData.Division = "10"; //  lets put that as a toyota dealer
+						receivedData.Attribute = "01"
+
+					}
+
+					if ((receivedData.BusinessPartner == legacyDealer || receivedData.SearchTerm2 == legacyDealer) && (userType == 'Dealer')) {
+						sendToUi.legacyDealer = receivedData.BusinessPartner,
+							sendToUi.legacyDealerName = receivedData.BusinessPartnerName
+						sendToUi.attributes.push(receivedData);
+						break;
+					}
+
+					if (userType == 'Dealer') {
+						continue;
+					} else {
+						sendToUi.attributes.push(receivedData);
+					}
+				}
+
+				res.type("application/json").status(200).send(sendToUi);
+
+			} else {
+
+				var result = JSON.stringify(body);
+				res.type('application/json').status(400).send(result);
+			}
+		});
+
+	});
 	return app;
 };
