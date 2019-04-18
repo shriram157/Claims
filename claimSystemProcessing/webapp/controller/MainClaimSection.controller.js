55909bbd8b2163cfcb7502cbbc39fc3b81c269d3
S0019470890 <minakshi_singh@toyota.ca>
2019-04-18 03:48:17 +0000

    changes done for security.

--- claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
+++ claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
@@ -777,21 +777,21 @@ sap.ui.define([
 							this.getView().getModel("DateModel").setProperty("/copyClaimEnable", true);
 							this.getView().getModel("DateModel").setProperty("/oDamageLineBtn", false);
 						}
-						// else if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTMR") {
-						// // 	//sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") == "Dealer_Services_Manager"
-						// 	this.getView().getModel("DateModel").setProperty("/oFormEdit", false);
-						// 	this.getView().getModel("DateModel").setProperty("/SaveClaim07", false);
-						// 	this.getView().getModel("DateModel").setProperty("/damageLine", false);
-						// 	this.getView().getModel("DateModel").setProperty("/updateEnable", false);
-						// 	this.getView().getModel("DateModel").setProperty("/copyClaimEnable", false);
-						// 	this.getModel("LocalDataModel").setProperty("/CancelEnable", false);
-						// 	this.getModel("LocalDataModel").setProperty("/UploadEnable", false);
-						// 	this.getView().getModel("DateModel").setProperty("/oDamageLineBtn", false);
-						// 	this.getView().getModel("DateModel").setProperty("/authAcClm", false);
-						// 	this.getView().getModel("DateModel").setProperty("/authRejClm", false);
-						// 	this.getView().getModel("DateModel").setProperty("/claimEditSt", true);
-						//  } 
 						else if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTMR") {
+						// 	//sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") == "Dealer_Services_Manager"
+							this.getView().getModel("DateModel").setProperty("/oFormEdit", false);
+							this.getView().getModel("DateModel").setProperty("/SaveClaim07", false);
+							this.getView().getModel("DateModel").setProperty("/damageLine", false);
+							this.getView().getModel("DateModel").setProperty("/updateEnable", false);
+							this.getView().getModel("DateModel").setProperty("/copyClaimEnable", false);
+							this.getModel("LocalDataModel").setProperty("/CancelEnable", false);
+							this.getModel("LocalDataModel").setProperty("/UploadEnable", false);
+							this.getView().getModel("DateModel").setProperty("/oDamageLineBtn", false);
+							this.getView().getModel("DateModel").setProperty("/authAcClm", false);
+							this.getView().getModel("DateModel").setProperty("/authRejClm", false);
+							this.getView().getModel("DateModel").setProperty("/claimEditSt", true);
+						 } 
+						else if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTMR" && sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") == "Dealer_Services_Manager") {
 							//sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") == "Dealer_Services_Manager"
 							this.getView().getModel("DateModel").setProperty("/oFormEdit", false);
 							this.getView().getModel("DateModel").setProperty("/SaveClaim07", false);
@@ -820,7 +820,8 @@ sap.ui.define([
 
 						}
 
-						if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTIC" && oClaimNav != "Inq") {
+						if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTIC" && oClaimNav != "Inq" && sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") != "Zone_User" && 
+						sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") != "TCI_Admin") {
 
 							//sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") != "Zone_User" && sap.ui.getCore().getModel(
 							//	"UserDataModel").getProperty("/LoggedInUser") != "TCI_Admin"
@@ -836,7 +837,8 @@ sap.ui.define([
 							this.getView().getModel("DateModel").setProperty("/authRejClm", false);
 							this.getView().getModel("DateModel").setProperty("/damageLine", true);
 							this.getView().getModel("DateModel").setProperty("/oDamageLineBtn", true);
-						} else if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTRC" && oClaimNav != "Inq") {
+						} else if (data.results[0].ProcessingStatusOfWarrantyClm == "ZTRC" && oClaimNav != "Inq" && sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") != "Zone_User" && 
+						sap.ui.getCore().getModel("UserDataModel").getProperty("/LoggedInUser") != "TCI_Admin") {
 							//	sap.ui.getCore().getModel(	"UserDataModel").getProperty("/LoggedInUser") != "Zone_User" && sap.ui.getCore().getModel("UserDataModel").getProperty(
 							//	"/LoggedInUser") != "TCI_Admin"
 							this.getView().getModel("DateModel").setProperty("/oFormEdit", true);
