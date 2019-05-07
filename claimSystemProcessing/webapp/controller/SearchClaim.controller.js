61b9044c21c9cfe40e98f9230a425339ed169570
S0019470890 <minakshi_singh@toyota.ca>
2019-05-07 14:22:07 +0000

    changes done for security

--- claimSystemProcessing/webapp/controller/SearchClaim.controller.js
+++ claimSystemProcessing/webapp/controller/SearchClaim.controller.js
@@ -61,8 +61,8 @@ sap.ui.define([
 					// var userScopes = oData;
 					// userScopes.forEach(function (data) {
 
-					//var userType = oData.loggedUserType[0];
-					var userType = "ManageAllShowAuthorization";
+					var userType = oData.loggedUserType[0];
+					//var userType = "ManageAllShowAuthorization";
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/LoggedInUser", userType);
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "");
 					switch (userType) {
