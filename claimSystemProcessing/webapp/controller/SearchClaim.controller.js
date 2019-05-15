f19f98c18859ea97432196492e1cade1e5f3919b
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-05-15 19:28:35 +0000

    committing changes for parts, change in odata label, PartQTY

--- claimSystemProcessing/webapp/controller/SearchClaim.controller.js
+++ claimSystemProcessing/webapp/controller/SearchClaim.controller.js
@@ -66,8 +66,8 @@ sap.ui.define([
 				dataType: "json",
 				success: function (oData) {
 
-					var userType = oData.loggedUserType[0];
-					//	var userType = "Dealer_Services_Admin";
+					// var userType = oData.loggedUserType[0];
+						var userType = "ManageAllParts";
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/LoggedInUser", userType);
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "");
 					switch (userType) {
@@ -164,8 +164,8 @@ sap.ui.define([
 						/*Uncomment for security*/
 						break;
 					default:
-						// console.log("Dealer_Services_Manager");
-						sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "ManageAllShowAuthorization");
+						// console.log("Dealer Parts");
+						sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "ManageAllParts");
 						/*Uncomment for security*/
 						that.getView().getModel("HeaderLinksModel").setProperty("/NewClaim", true);
 						that.getView().getModel("HeaderLinksModel").setProperty("/ViewUpdateClaims", true);
