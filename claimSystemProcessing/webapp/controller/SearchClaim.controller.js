16c679719ef9b90fb312c2c50635adcfabca7178
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-05-29 14:10:54 +0000

    commit- added fix for 12680-parts

--- claimSystemProcessing/webapp/controller/SearchClaim.controller.js
+++ claimSystemProcessing/webapp/controller/SearchClaim.controller.js
@@ -74,6 +74,7 @@ sap.ui.define([
 
 					var userType = oData.loggedUserType[0];
 					//var userType = "Dealer_Services_Admin";
+					// var userType = "ManageAllParts";
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/LoggedInUser", userType);
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "");
 					switch (userType) {
