0fd30123ae30db4828ef7f0908257e7c53ac1591
Minakshi Singh <minakshi_singh@toyota.ca>
2019-10-21 13:53:04 +0000

    small change done.

--- claimSystemProcessing/webapp/controller/QueryCoverageTools.controller.js
+++ claimSystemProcessing/webapp/controller/QueryCoverageTools.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([
 	return BaseController.extend("zclaimProcessing.controller.QueryCoverageTools", {
 		formatter: formatter,
 		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf zclaimProcessing.view.QueryCoverageTools
 		 */
