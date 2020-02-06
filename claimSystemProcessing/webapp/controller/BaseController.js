8f549aa6566a3793b9bf699b054fb6a2de6fc254
Minakshi Singh <minakshi_singh@toyota.ca>
2020-02-06 14:32:14 +0000

    changes done for defects.

--- claimSystemProcessing/webapp/controller/BaseController.js
+++ claimSystemProcessing/webapp/controller/BaseController.js
@@ -94,8 +94,8 @@ sap.ui.define([
 				type: "GET",
 				dataType: "json",
 				success: function (oData) {
-					//var userType = oData.loggedUserType[0];
-					var userType = "Dealer_Parts_Admin";
+					var userType = oData.loggedUserType[0];
+					//var userType = "Dealer_Parts_Admin";
 					//var userType = "Dealer_Parts_Services_Admin";
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/LoggedInUser", userType);
 					sap.ui.getCore().getModel("UserDataModel").setProperty("/UserScope", "");
