88f7af2ffa1542224bcf865f0c351999d063b01c
Minakshi Singh <minakshi_singh@toyota.ca>
2020-03-25 17:38:10 +0000

    changes done for approve reject.

--- claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
+++ claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
@@ -3691,7 +3691,7 @@ sap.ui.define([
 
 			};
 			this.getModel("LocalDataModel").setProperty("/oSavePartIndicator", true);
-			oClaimModel.update("/zc_headSet('" + oClaimNum + "')", obj, {
+			oClaimModel.update("/zc_headSet", obj, {
 				method: "PUT",
 				success: $.proxy(function (response) {
 					this.getModel("LocalDataModel").setProperty("/oSavePartIndicator", false);
@@ -3761,7 +3761,7 @@ sap.ui.define([
 						text: oBundle.getText("Yes"),
 						press: $.proxy(function () {
 							this.getModel("LocalDataModel").setProperty("/oSavePartIndicator", true);
-							oClaimModel.update("/zc_headSet('" + oClaimNum + "')", obj, {
+							oClaimModel.update("/zc_headSet", obj, {
 								method: "PUT",
 								success: $.proxy(function (response) {
 									this.getModel("LocalDataModel").setProperty("/oSavePartIndicator", false);
