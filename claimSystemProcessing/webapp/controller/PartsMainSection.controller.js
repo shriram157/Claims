e256bc30e24dd01280b2737c3c30d8df121ea33c
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2022-10-14 14:53:11 +0000

    Enabled uploader for ZPPD claim type

--- claimSystemProcessing/webapp/controller/PartsMainSection.controller.js
+++ claimSystemProcessing/webapp/controller/PartsMainSection.controller.js
@@ -539,7 +539,7 @@ sap.ui.define([
 					this.getModel("LocalDataModel").setProperty("/UploadEnable", false);
 
 					this.getView().getModel("multiHeaderConfig").setProperty("/multiheader5", 6);
-					this.getView().getModel("multiHeaderConfig").setProperty("/uploader", false);
+					this.getView().getModel("multiHeaderConfig").setProperty("/uploader", true);
 					this.getView().getModel("multiHeaderConfig").setProperty("/RetainPartV", true);
 					this.getView().getModel("multiHeaderConfig").setProperty("/PartNumberRcV", true);
 					this.getView().getModel("multiHeaderConfig").setProperty("/PartDescriptionOrdRcv", true);
