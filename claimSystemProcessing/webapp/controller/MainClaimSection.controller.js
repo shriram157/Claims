c07a1211904bf4a28d397d53e7046f5c8b01a28d
Vikas Reddy <vikas_reddy@toyota.ca>
2022-11-15 15:18:17 +0000

    Save Damage fields validation INC0222153

--- claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
+++ claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
@@ -6896,7 +6896,12 @@ sap.ui.define([
 				"DmgTypeCode": this.getView().getModel("HeadSetData").getProperty("/DmgTypeCode"),
 				"DmgSevrCode": this.getView().getModel("HeadSetData").getProperty("/DmgSevrCode")
 			};
-		//	if ("DmgAreaCode"!="" && "DmdTypeCode"!="" && "DmgSevrCode"!="") {               //changes by swetha
+			/* TODO: changes by Vikas -15-11-2022 for handling field level validation --Changes Start */
+			var sValidationPass = this.HandleFieldValidation(Object.values(itemObj));
+			if (sValidationPass=== true){
+				return MessageToast.show("Please enter all required fields");
+			}
+			/* TODO: changes by Vikas -15-11-2022 for handling field level validation -- Changes End */
 			this.obj.zc_claim_item_damageSet.results.push(itemObj);
 			this.getModel("LocalDataModel").setProperty("/oSavePartIndicator", true);
 			oClaimModel.refreshSecurityToken();
@@ -6928,6 +6933,13 @@ sap.ui.define([
 
 		//}
 			
+		},
+		/*
+		*changes by Vikas -
+		*15-11-2022 for handling field level validation
+		*/
+		HandleFieldValidation: function (sList){
+			return sList.findIndex(e => e === undefined) > -1;
 		},
 		onAddDamageLine: function () {
 			this.getView().getModel("DateModel").setProperty("/damageLine", true);
