a6a0a8bd3718837e638a464575bb6726b7da42c8
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2023-05-05 10:51:19 +0000

    changes for DMND0004037

--- claimSystemProcessing/webapp/utils/formatter.js
+++ claimSystemProcessing/webapp/utils/formatter.js
@@ -728,7 +728,9 @@ zclaimProcessing.utils.formatter = {
 		return Oval;
 
 	},
-	fnOutboundDeliveryNumber: function (WarrantyClaimType,sSelectedLocale) {                                //changes by swetha on 4th May, 2023 for DMND0004037
+	fnOutboundDeliveryNumber: function (WarrantyClaimType) {         //changes by swetha on 4th May, 2023 for DMND0004037
+		var sSelectedLocale;		
+		sSelectedLocale = window.location.search.match(/language=([^&]*)/i)[1];
 		if (WarrantyClaimType == "ZPDC" || WarrantyClaimType == "ZPTS" ) {
 			if(sSelectedLocale == "en"){
 				return "Outbound Delivery/Invoice Number";
