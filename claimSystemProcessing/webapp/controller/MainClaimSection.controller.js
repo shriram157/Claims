d55dff5cac795d70afef744a8178148ca16ac05b
Swetha Vatsavai <swetha_vatsavai@toyota.ca>
2023-09-06 15:40:02 +0000

    changes for DMND0003836

--- claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
+++ claimSystemProcessing/webapp/controller/MainClaimSection.controller.js
@@ -1815,9 +1815,9 @@ sap.ui.define([
 									oTableSelectedRow].AgreementNumber);
 							}
 							//changes by Swetha for DMND0003836 on 6th Sept, 2023 
-							if (this.getView().getModel("HeadSetData").getProperty("/RepairDate") > this.getView().getModel("HeadSetData").getProperty("/RepairCDate")){
-								MessageToast.show(oBundle.getText("RO open date cannot be greater than RO close date");	
-							}
+							//if (this.getView().getModel("HeadSetData").getProperty("/RepairDate") > this.getView().getModel("HeadSetData").getProperty("/RepairCDate")){
+							//	MessageToast.show(oBundle.getText("RO open date cannot be greater than RO close date");	
+							//}
 						}, this),
 						error: function () {}
 					});
