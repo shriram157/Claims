ccae0bfaaadc0010d31bc32307b0a9f4a113a33f
Shriram Sundaram <shriram_sundaram@toyota.ca>
2023-09-18 18:26:04 +0000

    //INC0239353  CPS quick coverage tool	  Shriram 18-SEPT-2023

--- claimSystemProcessing/webapp/controller/QueryCoverageTools.controller.js
+++ claimSystemProcessing/webapp/controller/QueryCoverageTools.controller.js
@@ -494,6 +494,8 @@ sap.ui.define([
 			this.getView().getModel("DateModel").setProperty("/foreignVinInd", false);
 			this.getView().getModel("DateModel").setProperty("/writtenOffInd", false);
 			this.getView().getModel("DateModel").setProperty("/specialVinInd", false);
+			
+			agreementno = '' //INC0239353  CPS quick coverage tool	  Shriram 18-SEPT-2023
 		}
 	});
 
